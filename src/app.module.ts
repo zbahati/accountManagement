50927b946e74b9d606e6dbe60cd8b3462a1216c9
zbahati <bahatizirimwabagabo@gmail.com>
2024-06-17 11:08:06 +0200

    add configuration of Typeorm to connect to the database pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+    useFactory: (configServie: ConfigService) => ({
+      type: 'postgres',
+      host: configServie.getOrThrow('HOST'),
+      port: configServie.getOrThrow('PORT'),
+      username: configServie.getOrThrow('USER'),
+      password: configServie.getOrThrow('PASSWORD'),
+      database: configServie.getOrThrow('DB'),
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  }),
+    UserModule
+  ],
 })
 export class AppModule {}
